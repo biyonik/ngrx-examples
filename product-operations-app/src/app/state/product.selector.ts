6a75f64f7e679e6f7fab1976170eda326fb9e239
biyonik <ahmet.altun60@gmail.com>
2024-07-17 15:51:25 +0300

    Selector mechanism completed

---
+++ product-operations-app/src/app/state/product.selector.ts
@@ -0,0 +1,20 @@
+import { createFeatureSelector, createSelector, State } from '@ngrx/store';
+import { ProductState } from './product.state';
+
+const getProductState =
+  createFeatureSelector<ProductState>('productStoreSlice');
+
+export const getProducts = createSelector(
+  getProductState,
+  (state: ProductState) => state.productList
+);
+
+export const getCurrency = createSelector(
+  getProductState,
+  (state: ProductState) => state.currency
+);
+
+export const getProductById = (id: number | string) =>
+  createSelector(getProductState, (state: ProductState) => {
+    return state.productList.find((p) => p.id === id);
+  });
